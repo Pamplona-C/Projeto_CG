edc56f14dd217f5c6b31b4ac62f4164e9b8023e8
pamplona <gustavo.pamplona.549@gmail.com>
2024-01-21 14:27:55 -0300

    Ajuste alg Bresenhan

--- AlgBres/src/algbres/Desenha.java
+++ AlgBres/src/algbres/Desenha.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package algbres;
 
 import java.awt.Color;
@@ -31,53 +28,51 @@ public class Desenha extends JFrame{
          g.setColor(Color.red);
         algBres(g,10,40,200,40);
         algBres(g, 10, 40, 10, 200);
+        algBres(g, 10, 40, 100, 100);
         algBres(g, 10, 200, 200, 200);
         algBres(g, 200, 200, 200, 40);
     }
 
     private void algBres(Graphics g, int xi, int yi, int xf, int yf) {
-        int x = xi, y = yi, d = 0, dx= xf-xi, dy = yf-yi, c, m, incX = 1, incY = 1;
-        
-        if (dx < 0) {
-            incX = -1;
-            dx = -dx; 
-        }
-        if (dy < 0) {
-            incY = -1;
-            dy = -dy;   
-        }
-        if(dy <= dx){
-            c = 2*dx;
-            m = 2*dy;
-            if (incX < 0) {
-                dx++;  
-            }
-            for (;;) {
-                putPixel(g, x, y);
-                if (x == xf)break;
-                x += incX;
-                d =+ m;
-                if ( d >= dx) {
-                    y =+ incY;
-                    d -= c;
-                }
-            }
-        }else{
-             c = 2*dy;
-            m = 2*dx;
-            if (incY  < 0) {
-                dy++;  
-            }
-            for (;;) {
-                putPixel(g, x, y);
-                if (y == yf)break;
-                y += incY;
-                d =+ m;
-                if ( d >= dy) {
-                    x =+ incX;
-                    d -= c;
-                }
-            }
-        }
+        int x = xi, y = yi, d=0, dx = xf-xi, dy = yf-yi, c, m, incX=1, incY=1;
+       
+       if(dx < 0) {incX = -1; dx = -dx;}
+       if(dy < 0) {incY = -1; dy = -dy;}
+     
+       if(dy <= dx) {
+           c = 2 * dx;
+           m = 2 * dy;
+         
+           if(incX < 0) dx++;
+           for(;;) {
+               putPixel(g,x,y);
+               if (x == xf) break;
+               x += incX;
+               d += m;
+               if(d >= dx) {
+                   y += incY;
+                   d -= c;
+               }
+              
+           }
+       } else {
+           c = 2 * dy; 
+           m = 2 * dx;
+          
+           
+           if(incY < 0) dy++;
+           
+           for(;;) {
+               putPixel(g,x,y);
+               if (y == yf) break;
+               y += incY;
+               d += m;
+               if(d >= dy) {
+                   x += incX;
+                   d -= c;
+               }
+               
+       }
+       }
     }
 }
